93c5abd0df9a016273b80d37178c9f426362228b
Nicolas Bellon <nicolas@foodvisor.io>
2022-01-25 18:26:39 +0100

    Fix: step.sh

--- step.sh
+++ step.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 git clone git@github.com:Foodvisor/initiative-synchronizer-ci.git
-xcodebuild -project FVInitiativeSynchronizer/FVInitiativeSynchronizer.xcodeproj -configuration Release
-FVInitiativeSynchronizer/build/Release/FVInitiativeSynchronizer
+xcodebuild -project initiative-synchronizer-ci/FVInitiativeSynchronizer.xcodeproj -configuration Release
+initiative-synchronizer-ci/build/Release/FVInitiativeSynchronizer
